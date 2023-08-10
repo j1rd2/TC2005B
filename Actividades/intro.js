c94d5e6aefefe39ab45eb54f4c4831e99a9af4ff
j1rd2 <chuchoramirez195@gmail.com>
2023-08-10 12:57:00 -0600

    JavaScript y variables

---
+++ Actividades/intro.js
@@ -0,0 +1,18 @@
+// variables, constantes, consola (log, info, warn, error, assert)
+
+//Variable decalration with var
+var best_seller = "Barbie";
+
+//Variable decalration with let (moder)
+let best_movie = "Oppenheimer";
+
+//Print the best movie variable
+console.log(best_movie);
+
+//Declare variables with cons
+const price = 50;
+
+console.log(price);
+
+
+
