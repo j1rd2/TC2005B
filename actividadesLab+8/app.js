e8e92163b19ddbc6ccc2af3201fdd5d8701a10a4
j1rd2 <chuchoramirez195@gmail.com>
2023-08-21 13:17:23 -0600

    Commit de todo lo llevo hasta el momento de la clase del lab 10

--- actividadesLab+8/app.js
+++ actividadesLab+8/app.js
@@ -38,9 +38,168 @@ const http = require('http');
 
 const server = http.createServer( (request, response) => {
     console.log(request.url);
-    response.setHeader('Content-Type', 'text/html')
-    response.write("Laboratorio 8 completado :)");
+    console.log(request.method);
+
+    if (request.url == "/") {
+
+        response.setHeader("Content-Type","text/html");
+        response.write(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="utf-8">
+        <title>Películas</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <header>
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="https://bulma.io">
+                        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28">
+                    </a>
+                
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+            </nav>
+        </header>
+        <main>
+            <section class="section">
+                <div class="container">
+                    <h1 class="title">Películas</h1>
+                    <p>
+                        <em>Cuando</em> <b>nosotros</b> <i>escribimos</i>  sobre <strong>películas</strong>
+                    </p>
+                    <br>
+                    <p>
+                        <label for="favorita">¿Cuál es tu película favorita?</label>
+                        <input id="favorita" class="input is-primary" type="text" placeholder="Oppenheimer">
+                    </p>
+                    <br>
+                    <h2 class="subtitle">En cartelera</h2>
+                    <button id="boton_cartelera" class="button is-info is-rounded">Ver cartelera</button>
+    
+                    <div id="posters"></div>
+    
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td id="Oppenheimer">Oppenheimer</td>
+                            </tr>
+                            <tr>
+                                <td style="color:pink">Barbie</td>
+                            </tr>
+                        </tbody>
+                        <thead>
+                            <tr>
+                                <th>Película</th>
+                            </tr>
+                        </thead>
+                        <tfoot>
+                            <tr>
+                                <td>Fecha: 10 de agosto de 2023</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+    
+                    <h3 class="subtitle">Mis favoritas</h3>
+                    <ol class="favorito">
+                        <li>Lobo de Wallstreet</li>
+                        <li>Django</li>
+                        <li>Madagascar</li>
+                    </ol>
+    
+                    <div id="ejercicio_1"></div>
+    
+                    <button id="boton_cartelera" class="button is-danger is-rounded">Botón en rama</button>
+    
+                </div>
+            </section>
+        </main>
+        <footer>
+    
+        </footer>
+        </body>
+        </html>
+
+    `);
     response.end();
+    } else if(request.url == "/new" && request.method == "GET"){
+        response.write(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8">
+            <title>Películas</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+        </head>
+        <body>
+            <header>
+                <nav class="navbar" role="navigation" aria-label="main navigation">
+                    <div class="navbar-brand">
+                        <a class="navbar-item" href="https://bulma.io">
+                            <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28">
+                        </a>
+                    
+                        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                            <span aria-hidden="true"></span>
+                        </a>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <section class="section">
+                    <div class="container">
+                        <h1 class="title">Registro de Peliculas</h1>
+                        <form action="/new" method="POST>
+                            <label for="nombre> Nombre de la pelicula </label>
+                            <input id="nombre" class"input" type="text" placeholder "Oppenheimer>
+                            <br>
+                            <label for="sinapsis"> Sinapsis </label>
+                            <textarea id="sinapsis" name="sinapsis" class="textarea" placeholder="La historia de la bomba atómica"></textarea>
+                            <br>
+                            <input id="registrar" name="registrar" type="submit" value="Registrar" class="button is-info">
+                        </form>
+                    </div>
+            </body>
+            </html>
+    
+        `);
+        response.end();
+    } else if(request.url == "/new" && request.method == "POST") { 
+        
+        const datos = [];
+
+        request.on('data', (dato) => {
+            // console.log(dato);
+            datos.push(dato);
+        });
+
+        request.on('end', () => {
+            const datos_completos = Buffer.concat(datos).toString();
+            console.log(datos_completos);
+            const primera_variable = datos_completos.split('&')[0];
+            console.log(primera_variable);
+            const primer_valor = primera_variable.split('=')[1];
+            console.log(primer_valor);
+            response.write(`La película fue registrada`);
+            return response.end();
+        })
+
+        // console.log(dato);
+        // response.end();
+    }
+    else {
+        response.statusCode = 404;
+    }
 
 });
 
