0be6a3fb42e76d022987a475050187cc8f9834a1
j1rd2 <chuchoramirez195@gmail.com>
2023-09-08 22:34:56 -0600

    Registrar rutas con GET y POST

--- Laboratorios/Lab10/app.js
+++ Laboratorios/Lab10/app.js
@@ -9,9 +9,71 @@ app.use((request, response, next) => {
     next() // Permite a la peticion avanzar al siguiente middleware
 });
 
+app.get('/tienda/vender', (request, response, next) => {
+    const html = `
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <title>Laboratorio</title>
+            <script src="https://cdn.tailwindcss.com"></script>
+        </head>
+        <body>
+            <header>
+                <nav class="bg-sky-900 p-4">
+                    <div class="container mx-auto flex justify-between items-center">
+                        <img class="justify-items-start w-20 h-20" src="logo.png"> 
+                        <h1 class="font-mono text-center text-2xl text-stone-50">LOVE BIKES</h1>
+                        <ul class="flex space-x-4">
+                            <li><a href="#" class="text-white">Home</a></li>
+                            <li><a href="#" class="text-white">About</a></li>
+                            <li><a href="#" class="text-white">Services</a></li>
+                            <li><a href="#" class="text-white">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+            <main>
+                <h1 class="font-mono text-center text-4xl text-black"> Quiero vender mi bici </h1>
+                <br>
+                <div class="flex justify-center">
+                    <div>
+                        <form action="/tienda/vender" method="POST">
+                            <label for="marca">Marca: </label>
+                            <input id="marca" name="marca" class="input" type="text" placeholder="Marca">
+                            <br><br>
+                            <label for="modelo">Modelo: </label>
+                            <input id="modelo" name="modelo" class="input" type="text" placeholder="Modelo">
+                            <br><br>
+                            <label for="anio">Año: </label>
+                            <input id="anio" name="anio" class="input" type="text" placeholder="Año aqui">
+                            <br><br>
+                            <label for="descripcion"> Descripcion: </label>
+                            <textarea id=descripcion" name="descripcion" class="textarea resize" placeholder="Escribe una breve descripcion aqui"></textarea>
+                            <br><br>
+                            <div class="text-center">
+                                <input class="bg-sky-900 hover:bg-blue-700 text-stone-50 py-2 px-4 rounded cursor-pointer mx-auto" id="registrar" name="registrar" type="submit" value="registrar">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </main>
+            <footer>
+            </footer>
+        </body>
+        </html>
+        `;
+    response.send(html);
+});
+
+app.post('/tienda/vender', (request, response, next) => {
+    console.log(request.body);
+    response.redirect('/');
+});
+
 app.use((request, response, next) => {
     console.log('Otro middleware :0');
-    response.send("aios"); // Manda la respuesta
+    response.send("Hello World"); // Manda la respuesta
 });
 
 app.listen(3000);
