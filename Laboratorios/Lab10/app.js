a55fdb626bcb78935526b677d1f44ac04f0029f0
j1rd2 <chuchoramirez195@gmail.com>
2023-09-08 22:53:15 -0600

    BodyParser instalado y configurado

--- Laboratorios/Lab10/app.js
+++ Laboratorios/Lab10/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+//Para usar body parser
+
+const bodyParser = require ('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+
 // Middleware
 
 app.use((request, response, next) => {
