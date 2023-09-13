f54a9f68e473ee127442dabcbd30e3d2d1848b2a
j1rd2 <chuchoramirez195@gmail.com>
2023-09-13 08:00:20 -0600

    Correccion de errores y ejecutar consulta desde modelo

--- Laboratorios/Lab10/models/tienda.model.js
+++ Laboratorios/Lab10/models/tienda.model.js
@@ -1,5 +1,7 @@
 let productos = []; // Arreglo de productos
 
+const db = require('../util/database');
+
 module.exports = class Productos {
     constructor(nuevoProducto) { // Constructor para construir el objeto y en que prioridad
         this.marca = nuevoProducto.marca;
@@ -16,6 +18,13 @@ module.exports = class Productos {
 
     // Metodo para devolver los objetos del objeto
     static fetchAll(){
-        return productos;
+        return db.execute ('SELECT * FROM lovebikes')
+        .then (([rows, fieldData]) => {
+            console.log(rows);
+            console.log(fieldData);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
 }
