6658487e196ce6a6436217111f0b7bfe9955b852
j1rd2 <chuchoramirez195@gmail.com>
2023-09-12 23:15:29 -0600

    Destruir sesion

--- Laboratorios/Lab10/controllers/users.controller.js
+++ Laboratorios/Lab10/controllers/users.controller.js
@@ -1,8 +1,14 @@
 exports.get_login = (request, response, next) => {
     response.render('users/login.ejs');
-}
+};
 
 exports.post_login = (request, response, next) => {
     request.session.username = request.body.username;
     response.redirect('/');
-}
+};
+
+exports.get_logout = (request, response, next) => {
+    request.session.destroy(()=>{
+        response.redirect('users/login'); // Se ejecuta cuando la sesion termina
+    })
+};
--- Laboratorios/Lab10/routes/users.routes.js
+++ Laboratorios/Lab10/routes/users.routes.js
@@ -5,5 +5,6 @@ const { route } = require('./principal.routes');
 
 router.get('/login', usersController.get_login);
 router.post('/login', usersController.post_login);
+router.get('/login', usersController.get_logout);
 
 module.exports = router;
