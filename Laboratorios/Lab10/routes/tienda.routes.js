51cbce2a9a1c7d90c7eaf88f2999838a1d5e3b08
j1rd2 <chuchoramirez195@gmail.com>
2023-09-13 17:59:33 -0600

    Se protegieron rutas

--- Laboratorios/Lab10/routes/tienda.routes.js
+++ Laboratorios/Lab10/routes/tienda.routes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 
 const tiendaController = require('../controllers/tienda.controller'); // Para controlador de tienda
 
-router.get('/tienda/vender', tiendaController.get_vender);
-router.post('/tienda/vender',tiendaController.post_vender);
-router.get('/tienda', tiendaController.get_tienda);
-router.get('/tienda/:id', tiendaController.get_tienda);
-router.get('/:id', tiendaController.get_tienda);
+const isAuth = require('../util/is-auth');
+
+router.get('/tienda/vender',isAuth, tiendaController.get_vender);
+router.post('/tienda/vender',isAuth, tiendaController.post_vender);
+router.get('/tienda',isAuth, tiendaController.get_tienda);
+router.get('/tienda/:id',isAuth, tiendaController.get_tienda);
+router.get('/:id',isAuth, tiendaController.get_tienda);
 
 
 module.exports = router;
